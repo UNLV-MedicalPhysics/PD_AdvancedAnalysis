cbac375a31b1b4dd957a614172e1813c4c2858dd
Matt <mattcschmidt@hotmail.com>
2017-07-14 16:22:19 -0700

    Handled the patient ID being null in the gamma pass table and the patient ID being incorrect in the grab patient button handler. Changed the Grab Course button event to the course_ddl SelectionChanged event and copide the button event code into the SelectionChanged event. If all works out, the button event code can eventually be removed.

--- PD_AdvAnalysis/GammaPassMatrix.xaml.cs
+++ PD_AdvAnalysis/GammaPassMatrix.xaml.cs
@@ -40,6 +40,11 @@ namespace PD_AdvAnalysis
         {
             //newcontext = System.Windows.Application.Current.MainWindow.FindName("newcontext") as Patient;
             newcontext = PD_AdvAnalysis.MainWindow.newcontext;
+            if(newcontext == null)
+            {
+                MessageBox.Show("Please input patient ID correctly");
+                return;
+            }
             field = PD_AdvAnalysis.MainWindow.field;
             gamma_grd.Children.Clear();
             //here's where all the magic happens!!
