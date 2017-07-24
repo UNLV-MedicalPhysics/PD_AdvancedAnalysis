6d7566e3f36a270b42165448587d6bf15c42ff49
aenea <aenea@LAPTOP-4B1QR15M>
2017-07-24 12:19:07 -0700

    Working on binding the circle to the slider button.

--- PD_AdvAnalysis/MainWindow.xaml.cs
+++ PD_AdvAnalysis/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -44,7 +46,7 @@ namespace PD_AdvAnalysis
         PortalDoseImage fieldm;
         PortalDoseImage fieldc;
         //load up an application
-        public VMS.DV.PD.Scripting.Application PDapp = VMS.DV.PD.Scripting.Application.CreateApplication(null,null);
+        public VMS.DV.PD.Scripting.Application PDapp;// = VMS.DV.PD.Scripting.Application.CreateApplication(null,null);
         public double to1;
         private IEnumerable<VMS.DV.PD.Scripting.EvaluationTestDesc> tested;
         //UserControl current_tab; 
--- PD_AdvAnalysis/WLAnalysis.xaml.cs
+++ PD_AdvAnalysis/WLAnalysis.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VMS.CA.Scripting;
 using VMS.DV.PD.Scripting;
+using System.IO;
 
 namespace PD_AdvAnalysis
 {
@@ -35,7 +38,7 @@ namespace PD_AdvAnalysis
 
 
             PDPlanSetup ps = PD_AdvAnalysis.MainWindow.plan;
-            foreach(PDBeam pb in ps.Beams)
+            foreach (PDBeam pb in ps.Beams)
             {
                 CheckBox cb = new CheckBox();
                 cb.Content = pb.Id;
@@ -48,9 +51,56 @@ namespace PD_AdvAnalysis
 
         }
 
-        private void manualydetect_btn_Click(object sender, RoutedEventArgs e)
-        {
+        // private void manualydetect_btn_Click(object sender, RoutedEventArgs e)
+        //{
+        //}
 
+        class EllipseConverter : IValueConverter
+        {
+            private void manualydetect_btn_Click(object sender, RoutedEventArgs e)
+            { }
+
+
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if (value == null) return null;
+
+                int radius = (int)value;
+                int diameter = radius * 2;
+
+                using (var bmp = new System.Drawing.Bitmap(diameter, diameter))
+                {
+                    using (var g = Graphics.FromImage(bmp))
+                    {
+                        g.FillEllipse(System.Drawing.Brushes.Blue,
+                                      0,
+                                      0,
+                                      diameter,
+                                      diameter);
+                    }
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bmp.Save(ms, ImageFormat.Bmp);
+                        ms.Position = 0;
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = ms;
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.EndInit();
+
+                        return bitmapImage;
+                    }
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
         }
     }
-}
+
+    
+
